189e9432d681a0e8bc01f4d692bec38aa3146188
DmtKravtsov <dmytrokravtsov55@gmail.com>
2026-04-17 19:38:01 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,59 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "users, expected",
+    [
+        (
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                }
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                }
+            ],
+        ),
+        (
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": None,
+                    "full_name": "Jack Holy",
+                }
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                }
+            ],
+        ),
+        (
+            [
+                {
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                }
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                }
+            ],
+        ),
+    ],
+)
+def test_restore_names(users, expected):
+    restore_names(users)
+    assert users == expected
